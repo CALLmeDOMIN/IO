66c911fd93893bc220fcebfbcef0e21568fe18c7
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-10-19 12:37:11 +0200

    first commit

---
+++ program.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int* BB;
+
+int main() {
+    int AA[10], ujemne;
+
+
+    for(int i = 0; i < 10; ++i){
+        std::cin>>AA[i];
+        if(AA[i] < 0)
+            ujemne++;
+    }
+
+    BB = new int[ujemne];
+    int iterator = 0;
+    for(int i = 0; i < 10; ++i){
+        if(AA[i] < 0){
+            BB[iterator] = AA[i];
+            iterator++;
+        }
+    }
+
+    for(int i = 0; i < ujemne; ++i)
+        std::cout<<BB[i]<<" ";
+
+    std::cout<<"Liczb jest: "<<ujemne<<" Pominieto: "<<10-ujemne;
+}
